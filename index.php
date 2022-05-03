ae5849919e1bf3ab23e3cfcd8eba3bfff217a727
BlackYukiOS <67216376+BlackYukiOS@users.noreply.github.com>
2022-05-03 22:56:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php $conf = $_GET['conf'];
-      if ($conf == 'mA2qO8iF2wC1wF5eJ7vC') { header ('location: Quantumult X/Rewrite/You+.conf');}?>
+      if ($conf == 'mA2qO8iF2wC1wF5eJ7vC') { header ('location: Quantumult X/Rewrite/You%2B.conf');}?>
